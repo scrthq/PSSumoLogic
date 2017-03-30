40842a685c31eb9828c4e0be9cf5523ba9ce1052
Nate Ferrell <nathaniel.ferrell@pnmac.com>
2017-03-30 10:48:59 -0700

    added limit and offset values to Get-SumoCollector

--- Public/Get-SumoCollector.ps1
+++ Public/Get-SumoCollector.ps1
@@ -5,6 +5,12 @@
       [string]
       $CollectorId,
       [parameter(Mandatory=$false)]
+      [int]
+      $Limit = 10000,
+      [parameter(Mandatory=$false)]
+      [int]
+      $Offset = 0,
+      [parameter(Mandatory=$false)]
       [ValidateNotNullOrEmpty()]
       [string]
       $Location = $Script:PSSumoLogic.Location,
@@ -27,6 +33,17 @@
     }
     else {
         $Prop = "collectors"
+    }
+    if ($Limit -or $Offset) {
+        if ($Limit -and $Offset) {
+            $URI = "$($URI)?limit=$Limit&offset=$Offset"
+        }
+        elseif ($Limit) {
+            $URI = "$($URI)?limit=$Limit"
+        }
+        elseif ($Offset) {
+            $URI = "$($URI)?offset=$Offset"
+        }
     }
     try {
         $response = Invoke-RestMethod -Method Get -Uri $URI -Headers $headers -ContentType "application/json" -ErrorAction Stop | Select-Object -ExpandProperty $Prop
